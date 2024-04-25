0cb293101797eeb1694c2a09aa126cbe39f1dea7
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-04-25 09:30:13 -0700

    Handling bad files

--- chap03/catr/src/main.rs
+++ chap03/catr/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -18,9 +20,19 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 fn run(args: Args) -> Result<()> {
     for filename in args.files {
-        println!("{filename}");
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {filename}: {err}"),
+            Ok(_) => println!("Opened {filename}"),
+        }
     }
     Ok(())
 }
