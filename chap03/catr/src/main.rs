84df83e4881ee7ceb02ed64eebce2a69a30f5152
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-04-25 09:15:54 -0700

    clap::Parser implementation

--- chap03/catr/src/main.rs
+++ chap03/catr/src/main.rs
@@ -1,61 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
 
-// #[derive(Debug, Parser)]
-// struct Args {
-//     /// Input text
-//     #[arg(required(true))]
-//     files: Vec<String>,
-
-//     /// Number all output lines
-//     #[arg(short('n'))]
-//     number_lines: bool,
-
-//     /// Number non-empty output lines
-//     #[arg(short('b'))]
-//     number_nonblank_lines: bool,
-// }
-
-#[derive(Debug)]
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `cat`
 struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+
+    /// Number lines
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank_lines"))]
     number_lines: bool,
+
+    /// Number non-blank lines
+    #[arg(short('b'), long("number-nonblank"))]
     number_nonblank_lines: bool,
 }
 
-fn get_args() -> Args {
-    let matches = Command::new("catr")
-        .version("0.1.0")
-        .author("Nikolai Schlegel <nikolai.schlegel@gmail.com>")
-        .about("Rust version of 'cat'")
-        .arg(
-            Arg::new("files")
-                .value_name("FILE")
-                .help("Input files")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("number_lines")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Number all output lines"),
-        )
-        .arg(
-            Arg::new("number_nonblank_lines")
-                .short('b')
-                .action(ArgAction::SetTrue)
-                .help("Number non-empty output lines"),
-        )
-        .get_matches();
-
-    Args {
-        files: matches.get_many("files").unwrap().cloned().collect(),
-        number_lines: matches.get_flag("number_lines"),
-        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
-    }
-}
-
 fn main() {
-    let args = get_args();
+    let args = Args::parse();
     println!("{args:#?}")
 }
